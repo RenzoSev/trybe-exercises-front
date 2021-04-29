6a11229c560f0d1612a80a3d8a6d01b7a8deb349
Renzo Sevilha <sevilharenzo@gmail.com>
2021-04-29 19:26:05 -0300

    [EX01] Feito

--- Bloco 12.1 Componentes com estado e evento/pokedex/src/App.js
+++ Bloco 12.1 Componentes com estado e evento/pokedex/src/App.js
@@ -3,13 +3,36 @@ import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.handlePokemon = this.handlePokemon.bind(this);
+    this.state = {
+      pokemonPosition: 0,
+    }
+  }
+
+  handlePokemon() {
+    if(this.state.pokemonPosition === pokemons.length - 1) {
+      this.setState((oldState, _props) => ({pokemonPosition: 0}))
+    }else {
+      this.setState((oldState, _props) => ({
+        pokemonPosition: oldState.pokemonPosition + 1
+      }))
+    }
+  }
+  
+  render () {
+    console.log(this.state.pokemonPosition);
+    return (
+      <div className="App">
+        <h1> Pokedex </h1>
+        <Pokedex pokemons={pokemons} position={this.state.pokemonPosition}/>
+        <button onClick={this.handlePokemon}>Next</button>
+      </div>
+    );
+  }
 }
 
 export default App;
