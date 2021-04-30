5b1a3e4d62fb2a3793cd61512ab3471e69826d73
Renzo Sevilha <sevilharenzo@gmail.com>
2021-04-30 00:19:29 -0300

    [COMMITAO BOMBA] Exercício do dia feito.

--- Bloco 12.1 Componentes com estado e evento/pokedex/src/App.js
+++ Bloco 12.1 Componentes com estado e evento/pokedex/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
 import pokemons from './data';
-import Pokedex from './Pokedex';
+
+import Pokedex from './components/Pokedex';
+import Button from './components/Button';
 
 class App extends React.Component {
   constructor() {
@@ -10,26 +12,62 @@ class App extends React.Component {
     this.handlePokemon = this.handlePokemon.bind(this);
     this.state = {
       pokemonPosition: 0,
+      arrayPokemon: pokemons
     }
   }
 
   handlePokemon() {
-    if(this.state.pokemonPosition === pokemons.length - 1) {
-      this.setState((oldState, _props) => ({pokemonPosition: 0}))
-    }else {
-      this.setState((oldState, _props) => ({
+    if(this.state.pokemonPosition === pokemons.length - 1) 
+      this.setState(() => ({pokemonPosition: 0}))
+     else {
+      this.setState((oldState) => ({
         pokemonPosition: oldState.pokemonPosition + 1
       }))
     }
   }
+
+  filterPokemons(specificType) {
+    const newArrayPokemon = specificType === 'All'
+     ? pokemons 
+     : pokemons.filter(({ type }) => specificType === type);
+   
+     this.setState(() => (
+      { 
+        arrayPokemon: newArrayPokemon,
+        numberOfClicks: 0
+      }
+      ));
+  }
   
   render () {
-    console.log(this.state.pokemonPosition);
+    const types = pokemons.reduce((array, { type }) => {
+      if(array.includes(type)) return array
+      else return array.concat(type)
+    }, ['All']);
+
     return (
       <div className="App">
         <h1> Pokedex </h1>
-        <Pokedex pokemons={pokemons} position={this.state.pokemonPosition}/>
-        <button onClick={this.handlePokemon}>Next</button>
+        <Pokedex pokemons={this.state.arrayPokemon} position={this.state.pokemonPosition}/>
+        <div className='pokeContainer'>
+          
+          <button 
+            onClick={this.handleClick}
+            disabled={ this.state.numberOfClicks === this.state.arrayPokemon.length - 1}
+            >
+            Next
+          </button>
+         
+          {types.map((type)=> (
+              <Button 
+              key={type} 
+              onClick={() => this.filterPokemons(type)} 
+              type={type}
+              className={'pokeButao'}
+              />
+            ))}
+         
+          </div>
       </div>
     );
   }
